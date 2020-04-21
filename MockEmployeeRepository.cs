1439ca3051c976f354f3de41c2e576f419d843d6
Vinit Singh <vinitsingh227@gmail.com>
2020-04-21 20:58:06 +0530

    Add files via upload

---
+++ MockEmployeeRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private static List<Employee> employeesList;
+        public MockEmployeeRepository()
+        {
+            employeesList = new List<Employee>()
+            {
+                new Employee() {Id=101, Name="Vinit", Email="vks@gmail.com", Department=Dept.IT },
+                new Employee() {Id=102, Name="Amit", Email="ak@gmail.com", Department=Dept.HR },
+                new Employee() {Id=103, Name="Sumit", Email="su@gmail.com", Department=Dept.IT },
+                new Employee() {Id=104, Name="Ravi", Email="ra@gmail.com", Department=Dept.Payroll }
+            };
+        }
+
+        public Employee Add(Employee employee)
+        {
+            employee.Id = employeesList.Max(e => e.Id) + 1;
+            employeesList.Add(employee);
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return employeesList;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return employeesList.FirstOrDefault(e => e.Id == id);
+        }
+        public Employee Delete(int id)
+        {
+           Employee employee = employeesList.FirstOrDefault(e => e.Id == id);
+            if(employee != null)
+            {
+                employeesList.Remove(employee);
+            }
+            return employee;
+        }
+
+        public Employee Update(Employee employeeChnage)
+        {
+            Employee employee = employeesList.FirstOrDefault(e => e.Id == employeeChnage.Id);
+            if (employee != null)
+            {
+                employee.Name = employeeChnage.Name;
+                employee.Email = employeeChnage.Email;
+                employee.Department = employeeChnage.Department;
+            }
+            return employee;
+        }
+    }
+}
---
+++ SQLEmployeeRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class SQLEmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppContexts _appContexts;
+
+        public SQLEmployeeRepository(AppContexts appContexts)
+        {
+            this._appContexts = appContexts;
+        }
+        public Employee Add(Employee employee)
+        {
+            //Add method add teh changes 
+            _appContexts.Employees.Add(employee);
+            //SaveChanges method save the details in teh database
+            _appContexts.SaveChanges();
+            return employee;
+        }
+
+        public Employee Delete(int id)
+        {
+            Employee employee = _appContexts.Employees.Find(id);
+            if(employee!= null)
+            {
+                _appContexts.Employees.Remove(employee);
+                _appContexts.SaveChanges();
+            }
+
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _appContexts.Employees;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _appContexts.Employees.Find(id);
+        }
+
+        public Employee Update(Employee employeeChnages)
+        {
+            var employee = _appContexts.Employees.Attach(employeeChnages);
+            employee.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _appContexts.SaveChanges();
+            return employeeChnages;
+        }
+    }
+}
